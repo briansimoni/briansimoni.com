0accee1a34eadce46f8e7d293c71944bdcdb26a3
Brian Simoni <simonibc@vcu.edu>
2016-08-30 19:59:09 -0400

    Add 404 page

---
+++ wp-content/themes/simoni/404.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brian
+ * Date: 8/30/16
+ */
+
+get_header();
+
+global $post;
+
+
+?>
+
+    <div id="content">
+        <div class="container">
+            <h1>404 - Page Not Found</h1>
+            <small>You probably made a typo</small>
+            <a href="http://play.typeracer.com/">link</a>
+        </div>
+    </div>
+
+<?php get_footer(); ?>
