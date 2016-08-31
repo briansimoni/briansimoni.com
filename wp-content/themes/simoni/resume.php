9ce57f174d06c5b461a3c92b8397e4dc5df6b1d5
Brian Simoni <simonibc@vcu.edu>
2016-08-31 09:00:52 -0400

    Add resume page

---
+++ wp-content/themes/simoni/resume.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: Resume
+ *
+ */
+
+get_header(); ?>
+
+<div id="content">
+    <div class="container">
+    <embed style="height:100vh; width:100%; margin: 30px 0;" src="/wp-content/themes/simoni/brian-simoni-resume-2016.pdf" width=”600″ height=”500″ alt=”pdf” pluginspage=”http://www.adobe.com/products/acrobat/readstep2.html”>
+    </div>
+</div>
+
+<?php get_footer() ?>
