1ceccd9afd2c28f5000a0d9431e1df7de7b77b60
Brian Simoni <simonibc@vcu.edu>
2016-09-08 14:27:52 -0400

    !

--- wp-content/themes/simoni/404.php
+++ wp-content/themes/simoni/404.php
@@ -16,7 +16,7 @@ global $post;
         <div class="container">
             <h1>404 - Page Not Found</h1>
             <p><small>You probably made a typo</small></p>
-            <p><a href="http://play.typeracer.com/">link</a></p>
+            <p><a href="http://play.typeracer.com/">link!</a></p>
         </div>
     </div>
 
