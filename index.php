84e4536bfce3c09cef826b614ca81cce509b10af
briansimoni <simonibc@vcu.edu>
2020-01-03 17:14:27 -0500

    Just keep the theme

--- index.php
+++ index.php
@@ -1,17 +1,88 @@
 <?php
 /**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
+ * Created by PhpStorm.
+ * User: brian
+ * Date: 8/26/16
+ * Time: 12:52 PM
  */
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+get_header();
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+?>
+<div id="content" class="container homepage-content">
+    <div class="row section-3-pictures">
+
+        <div class="col-md-4">
+            <div>
+                <a href="compsci">
+                    <img class="img-circle" src="/wp-content/themes/simoni/images/binary.png">
+                </a>
+            </div>
+            <p>Studying Computer Science at Virginia Commonwealth University</p>
+        </div>
+
+
+        <div class="col-md-4">
+            <div>
+                <a href="software">
+                    <img class="img-circle" src="/wp-content/themes/simoni/images/golang.png">
+                </a>
+            </div>
+            <p>Enthusiast for Software Engineering and Development</p>
+        </div>
+
+
+        <div class="col-md-4">
+            <div>
+                <a href="ems">
+                    <img class="img-circle" src="/wp-content/themes/simoni/images/ambulance.jpg">
+                </a>
+            </div>
+            <p>Volunteer Emergency Medical Technician</p>
+        </div>
+    </div>
+
+    <?php
+    global $wpdb;
+    $latest_posts = $wpdb->get_results("SELECT * FROM wp_posts WHERE (post_type = 'computer-science' 
+                                      OR post_type = 'fire-and-ems' OR post_type = 'software-development')
+                                      AND post_status = 'publish' ORDER BY post_date;");
+
+    $last = count($latest_posts) - 1;
+    $most_recent_post = $latest_posts[$last];
+
+    ?>
+
+    <div class="latest-post-div post">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Latest Post</h2>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <a href="<?php echo $most_recent_post->guid ?>">
+                    <h3><?php echo $most_recent_post->post_title ?></h3>
+                </a>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-4">
+                <div class="post-image">
+                    <a href="<?php echo $most_recent_post->guid ?>">
+                        <?php echo get_the_post_thumbnail( $most_recent_post ) ?>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-8"><p><?php echo $most_recent_post->post_excerpt ?></p></div>
+        </div>
+    </div>
+</div>
+
+
+
+<?php
+
+get_footer();
